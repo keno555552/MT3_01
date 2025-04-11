ea026cb3cf12d2abff664a8772d31b30863fff28
Kwan Kin Leung <kwankinleung5@gmail.com>
2025-04-11 15:39:52 +0900

    v1.0

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <Novice.h>
+﻿#include <Novice.h>
+#include "myMath.h"
 
 const char kWindowTitle[] = "GC01_05_カン_ケンリャン";
 
@@ -8,26 +9,35 @@ int kWindowWidth = 1280, kWindowHeight = 720;
 /// 重要定義、構造体作り
 ///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 
-typedef struct Vector2 {
-	float X;
-	float Y;
-} Vector2;
-
 enum stage { Title = 0, Stage, scoreBoard };
 int stageHandle = Title;
 
+// キー入力結果を受け取る箱
+char keys[256] = { 0 };
+char preKeys[256] = { 0 };
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
 
 	///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 	/// 初期化
 	///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+
+	int kRowHeight = 20;
+
+	Matrix4x4 orthographicMatrix =
+		MakeProjectionMatrix(-160.f, 200.0f, 160.f, 300.0f, 0.0f, 1000.0f);
+
+	Matrix4x4 perspectiveFovMatrix =
+		MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+
+	Matrix4x4 viewportMatrix =
+		MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 1.0f, 0.0f);
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -45,6 +55,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///=========================================================================================================================================================================================
 		/// 更新処理
 		///=========================================================================================================================================================================================
+
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 
 		///=========================================================================================================================================================================================
 		/// 描画処理
